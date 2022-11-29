1101126d435f0e760d6e7c8b8ae49bc2ef54caa9
Moongazing <tunali_2@outlook.com>
2022-11-29 14:09:57 +0300

    SelectFromMultipleInputSequences

--- Linq-Examples/Program.cs
+++ Linq-Examples/Program.cs
@@ -19,6 +19,7 @@ namespace Linq_Examples
       TakeWhile();
       CompareTwoSequencesForEquality();
       CompareSequencesWithZip();
+      SelectFromMultipleInputSequences();
     }
     public static void FilterElementBasedOnPosition()
     {
@@ -135,6 +136,22 @@ namespace Linq_Examples
 
       int dotProduct = vectorA.Zip(vectorB ,(a,b)=>a*b).Sum();
       Console.WriteLine($"Dot Product:{dotProduct}");
+    }
+    public static void SelectFromMultipleInputSequences()
+    {
+      int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+      int[] numbersB = { 1, 3, 5, 7, 8 };
+
+      var pairs = from a in numbersA
+                  from b in numbersB
+                  where a < b
+                  select (a, b);
+
+      Console.WriteLine("Pairs where a < b :");
+      foreach (var pair in pairs)
+      {
+        Console.WriteLine(pairs);
+      }
     }
 
   }
